1c8fc198f0628ed9b585763548213ac024642b35
leonoxo <alex71.sander71@gmail.com>
2020-03-27 20:01:51 -0300

    create user logged and user guest pages

--- app/screens/Account/MyAccount.jsx
+++ app/screens/Account/MyAccount.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import * as firebase from "firebase";
 import { View, Text } from "react-native";
+import Loading from "../../components/Loading";
+import UserGuest from "./UserGuest";
+import UserLogged from "./UserLogged";
 
 export default function MyAccount() {
   const [login, setLogin] = useState(null);
@@ -13,24 +16,8 @@ export default function MyAccount() {
   }, []);
 
   if (login === null) {
-    return (
-      <View>
-        <Text>Cargando</Text>
-      </View>
-    );
+    return <Loading isVisible={true} text="Cargando" />;
   }
 
-  if (login) {
-    return (
-      <View>
-        <Text>Usuario logiado</Text>
-      </View>
-    );
-  }
-
-  return (
-    <View>
-      <Text>Usuario no legado</Text>
-    </View>
-  );
+  return login ? <UserLogged /> : <UserGuest />;
 }
